0c1edba211c32270bd14efdd5c3016c408d6a0cb
russellderoeper <deror007@gmail.com>
2026-02-21 15:27:24 +0000

    cleaned up forward pass code.

--- src/memory_layer.py
+++ src/memory_layer.py
@@ -63,36 +63,32 @@ class MemoryPlusLayer(nn.Module):
 
 
     def lookup_memory(self, query):
-            # TODO: Compute Product Key Mechanism and retrieval
 
-            # 1. Apply normalisation for cosine similarity style lookup
-            k1 = self.qk_norm(self.subkey_one)
-            k2 = self.qk_norm(self.subkey_two)
+        # 1. Apply normalisation for cosine similarity style lookup
+        k1 = self.qk_norm(self.subkey_one)
+        k2 = self.qk_norm(self.subkey_two)
 
-            # 2. Get similarity subkey scores with query
-            sim_scores_1 = query @ k1.T
-            sim_scores_2  = query @ k2.T
+        # 2. Get similarity subkey scores with query
+        sim_scores_1 = query @ k1.T
+        sim_scores_2  = query @ k2.T
+        all_scores = sim_scores_1.unsqueeze(-1) + sim_scores_2.unsqueeze(-2)
 
-            # NOTE: Which should I use??
-            all_scores = sim_scores_1.unsqueeze(-1) + sim_scores_2.unsqueeze(-2)
+        # 3. Cartesian Product Search:
+        all_scores = all_scores.view(*all_scores.shape[:-2], -1) 
+        
+        # 4. Select the final top-k combinations
+        top_k_scores, top_k_indices = torch.topk(all_scores, self.top_k, dim=-1)
+        
+        # 5. Retrieve Values and Aggregate 
+        s = self.softmax(top_k_scores) 
 
-            # 3. Cartesian Product Search:
-            all_scores = all_scores.view(*all_scores.shape[:-2], -1) 
-            
-            # 4. Select the final top-k combinations
-            top_k_scores, top_k_indices = torch.topk(all_scores, self.top_k, dim=-1)
-            
-            # 5. Retrieve Values and Aggregate 
-            s = self.softmax(top_k_scores) 
-
-            # 6. Gather Values and Aggregate
-            # NOTE: Not sure if this is correct. I know embeddingbag is not efficient for large sizes. 
-            flat_indices = top_k_indices.view(-1, self.top_k)
-            flat_weights = s.view(-1, self.top_k)
-            y_flat = F.embedding_bag(flat_indices, self.values, per_sample_weights=flat_weights, mode='sum')
-            
-            
-            return y_flat.view(*query.shape[:-1], self.value_dim)
+        # 6. Gather Values and Aggregate
+        # NOTE: Not sure if this is correct. I know embedding-bag is not efficient for large memory slots. 
+        flat_indices = top_k_indices.view(-1, self.top_k)
+        flat_weights = s.view(-1, self.top_k)
+        y_flat = F.embedding_bag(flat_indices, self.values, per_sample_weights=flat_weights, mode='sum')
+        
+        return y_flat.view(*query.shape[:-1], self.value_dim)
 
     def forward(self, x):
 
