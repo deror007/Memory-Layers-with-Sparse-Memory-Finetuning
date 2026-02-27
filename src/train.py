81eff024a6e54d648b053f1779910c866f6e0ffe
russellderoeper <deror007@gmail.com>
2026-02-27 11:51:59 +0000

    re-formatted train.py

--- src/train.py
+++ src/train.py
@@ -10,7 +10,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from torch.profiler import profile, record_function, ProfilerActivity
 import timm
@@ -21,14 +20,15 @@ import numpy as np
 import gc
 from torch.utils.data import Dataset, DataLoader, Subset
 
+# PYTORCH_ALLOC_CONF=expandable_segments:True enabled to prevent fragmentation during this secondary training phase. For Google Colab.
+
 """
 TODO:
 1. [] Implement Sparse Memory finetuning using Lin et. al (2025) paper.
-    [] Modify this so that only the forward activations from Memory Layer onwards
-        ... gets stored!
-    [] Progressive Data-dropout implementation.
-2. [] create training set from fashion mnist dataset by only including misclasssifications.
-3. [] compare performance to pre-trained baseline model.
+    - [] Modify this so that only the forward activations from Memory Layer onwards gets stored!
+    - [] Progressive Data-dropout implementation.
+2. [x] create training set from fashion mnist dataset by only including misclassifications.
+3. [] compare performance to pre-trained baseline memory model.
 4. [] probably use avalanche for split long-tail classification dataset, and redo finetuning on different classes.
 """
 
@@ -104,7 +104,6 @@ def create_finetune_datasets(model, loader, device):
     
     return finetune_set, stable_set
 
-
 def train_epoch(model, loader, criterion, optimizer, device):
     model.train()
     running_loss, correct, total = 0.0, 0, 0
@@ -134,7 +133,6 @@ def train_epoch(model, loader, criterion, optimizer, device):
 
     return running_loss / len(loader), 100. * correct / total
 
-
 def validate(model, loader, criterion, device):
     model.eval()
     running_loss, correct, total = 0.0, 0, 0
@@ -149,68 +147,34 @@ def validate(model, loader, criterion, device):
             correct += predicted.eq(labels).sum().item()
     return running_loss / len(loader), 100. * correct / total
 
-def init_model(m_type, device):
-    if m_type == 'dense':
-        model = timm.create_model('vit_tiny_patch16_224', pretrained=False, num_classes=10, cache_dir="./models_dir").to(device)
-
-    else:
-        model = timm.create_model('vit_tiny_patch16_224', pretrained=False, num_classes=10, cache_dir = "./models_dir").to(device)
-        d_model = model.embed_dim
-        memory_slots = 256**2
-        model.blocks[6].mlp = MemoryPlusLayer(d_model=d_model, memory_slots=memory_slots).to(device)
-
-    return model
-
-def run_comparison(train_loader, test_loader, device, epochs=5, total_trials = 3):
+def sparse_memory_finetuning(model, train_loader, test_loader, device, epochs, seed):
     # Detect device (Note: MPS for Mac is an option, but profiler support varies)
 
-    # Fixed keys to match your storage logic
-    results = {'dense': {'val_loss': [], 'val_acc': []}, 'memory': {'val_loss': [], 'val_acc': []}}
-    speed_comp = {'dense_model' : 0.0, 'memory_model' : 0.0}
+    # print(f"\nStarting training for {name}...")
 
-    for t in range(total_trials):
-
-        set_seed(42+t)
-
-        for name in ['dense', 'memory']:
-
-            torch.cuda.empty_cache()
-            gc.collect()
-
-            print(f"\nStarting training for {name}...")
+    trial_accs = []
+    optimizer = optim.AdamW(model.parameters(), lr=1e-4)
+    criterion = nn.CrossEntropyLoss()
+    start_time = time.time()
 
 
-            model = init_model(name, get_device())
-
-            trial_accs = []
-            optimizer = optim.AdamW(model.parameters(), lr=1e-4)
-            criterion = nn.CrossEntropyLoss()
-            start_time = time.time()
-
-
-            for epoch in range(epochs):
-                print(f"Epoch {epoch+1}/{epochs}")
-                train_loss, train_acc = train_epoch(model, train_loader, criterion, optimizer, device)
-                print(f"Epoch {epoch+1}: train loss: {train_loss}, train acc: {train_acc}")
-                val_loss, val_acc = validate(model, test_loader, criterion, device)
-                print(f"Epoch {epoch+1}: Val Acc {val_acc:.2f}%")
-                trial_accs.append(val_acc)
-
-            results[name]['val_acc'].append(trial_accs)
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1}/{epochs}")
+        train_loss, train_acc = train_epoch(model, train_loader, criterion, optimizer, device)
+        print(f"Epoch {epoch+1}: train loss: {train_loss}, train acc: {train_acc}")
+        val_loss, val_acc = validate(model, test_loader, criterion, device)
+        print(f"Epoch {epoch+1}: Val Acc {val_acc:.2f}%")
+        trial_accs.append(val_acc)
 
 
-            end_time = time.time()
-            speed_comp[model] = end_time - start_time
+    end_time = time.time()
+    speed = end_time - start_time 
 
-            # Memory Cleanup between models
 
-            if name == 'memory' and t == 0:
-                torch.save(model.state_dict(), "./models_dir/tiny_vit_memory_plus.pt")
-
-            del model, optimizer
-            gc.collect()
-            torch.cuda.empty_cache()
+    # # del model, optimizer <- do this memory cleanup after and before when doing it multiple times.s
+    # gc.collect()
+    # torch.cuda.empty_cache()
 
 
 
-    return results, speed_comp
+    return model,train_acc, val_acc, speed
